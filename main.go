82080b2cf899363ebdb96beb416119bc992b8cc1
EyuAtske <EyuelA.Fekade21@gmail.com>
2026-04-16 10:59:15 +0300

    Can track how many request are being made

--- main.go
+++ main.go
@@ -3,15 +3,49 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
 func main() {
+	var apicfg apiConfig
 	servermux := http.NewServeMux()
-	servermux.Handle("/", http.FileServer(http.Dir(".")))
+	servermux.Handle("/app/", http.StripPrefix("/app", apicfg.middlewareMetricsInc(http.FileServer(http.Dir(".")))))
 	server := &http.Server{
 		Handler: servermux,
 		Addr: ":8080",
 	}
+	servermux.HandleFunc("/healthz", handleReadiness)
+	servermux.HandleFunc("/metrics", apicfg.handleMetrics)
+	servermux.HandleFunc("/reset", apicfg.handleReset)
 	err := server.ListenAndServe()
 	fmt.Println(err)
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func handleReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) handleMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %d\n", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) handleReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hits reset to 0\n"))
 }
